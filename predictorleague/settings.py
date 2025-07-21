4444385370c2b8a591545a0ecec750f438083e9d
Alex Steel <99911854+Alex-Steel-13@users.noreply.github.com>
2025-07-21 18:58:12 +0100

    adding static files

--- predictorleague/settings.py
+++ predictorleague/settings.py
@@ -51,8 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'predictorleague.urls'
