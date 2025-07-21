ed5b69a71ee4e887d71bac8db249afa6e94d41a1
Alex Steel <99911854+Alex-Steel-13@users.noreply.github.com>
2025-07-21 18:55:05 +0100

    adding whitenoise

--- predictorleague/settings.py
+++ predictorleague/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'predictorleague.urls'
