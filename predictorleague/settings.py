d1571e67fd27fe2e875f9490a62e43827bddcf3d
Alex Steel <99911854+Alex-Steel-13@users.noreply.github.com>
2025-07-28 20:12:44 +0100

    Switching to MySQL database

--- predictorleague/settings.py
+++ predictorleague/settings.py
@@ -80,8 +80,15 @@ WSGI_APPLICATION = 'predictorleague.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'AlexSteel$default',
+        'USER': 'AlexSteel',
+        'PASSWORD': 'Clara12345',
+        'HOST': 'AlexSteel.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
