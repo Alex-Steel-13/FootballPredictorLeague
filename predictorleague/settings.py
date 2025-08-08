95c74bd670a03b5c68fc589c8fb80835a183d793
Alex-Steel-13 <AlexSteel@5e604d9b56f1>
2025-08-08 17:18:15 +0000

    making ti work on pythonanywhere

--- predictorleague/settings.py
+++ predictorleague/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import Config, RepositoryEnv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = BASE_DIR / '.env'
+config = Config(RepositoryEnv(str(env_path)))
 
 
 # Quick-start development settings - unsuitable for production
