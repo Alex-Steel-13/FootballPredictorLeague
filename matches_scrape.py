af8017610fd7498f7b7de0bc94c05bf6b84e1efb
Ice659 <alexandersteel13@gmail.com>
2025-08-27 17:01:33 +0200

    stupid changes

--- matches_scrape.py
+++ matches_scrape.py
@@ -1,12 +1,5 @@
 import os
-import django
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'predictorleague.settings'
-django.setup()
-
 
 from predictions.models import Match
 
