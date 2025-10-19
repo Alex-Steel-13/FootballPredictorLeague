1a88acb5f43798cc00c5579e096e8c7f7ce18741
Alex Steel <alexsteel@Alexs-MacBook-Air.local>
2025-10-19 20:03:35 +0100

    fixing floating point integers

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -97,7 +97,7 @@ def leaderboard(request):
 
         entry["average_points_per_game"] =  round(entry["score"] / entry["number_of_played_predictions"]) if entry["number_of_played_predictions"] else 0
 
-        entry["win_prediction_ratio"] = str(round(entry["correct_winner"] / entry["number_of_played_predictions"], 2)*100) + "%" if entry["number_of_played_predictions"] else 0
+        entry["win_prediction_ratio"] = str(round((entry["correct_winner"] / entry["number_of_played_predictions"]*100))) + "%" if entry["number_of_played_predictions"] else 0
 
         entry["non_string_ratio"] = round(entry["correct_winner"] / entry["number_of_played_predictions"], 2)*100 if entry["number_of_played_predictions"] else 0   
 
