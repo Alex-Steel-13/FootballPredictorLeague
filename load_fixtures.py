0179b83a8085b2775b9a6926915920f9ef8ce7db
Alex Steel <alexsteel@Alexs-MacBook-Air-157.local>
2026-02-25 15:54:55 +0000

    matches march 2

--- load_fixtures.py
+++ load_fixtures.py
@@ -2,7 +2,7 @@ import csv
 from predictions.models import Match
 
 def run():
-    with open('scraped_matches/matches_feb.csv', newline="", encoding="utf-8") as csvfile:
+    with open('scraped_matches/matches_mar.csv', newline="", encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             Match.objects.create(
