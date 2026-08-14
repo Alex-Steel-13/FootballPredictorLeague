61fbc243de6e944db43e5ac89130ef6abb1b2484
Alex Steel <alexsteel@Alexs-MacBook-Air-1433.local>
2026-08-14 15:07:01 +0100

    helppp

--- delete_matches.py
+++ delete_matches.py
@@ -1,10 +1,16 @@
-from predictions.models import Match
 import os
-
 import django
+
+# 1. Configure settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "predictorleague.settings")
+
+# 2. Setup Django
 django.setup()
+
+# 3. Import models (This MUST be below django.setup!)
 from predictions.models import Match
+
 def run():
     for match in Match.objects.all():
         match.delete()
+    print("All matches deleted successfully!")
