758b29abe4123cd6e4019bd2f3fd4e764873b3b1
Alex Steel <alexsteel@Alexs-MacBook-Air-1433.local>
2026-08-14 15:06:18 +0100

    again as below I keep making errors

--- delete_matches.py
+++ delete_matches.py
@@ -4,6 +4,7 @@ import os
 import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "predictorleague.settings")
 django.setup()
+from predictions.models import Match
 def run():
     for match in Match.objects.all():
         match.delete()
