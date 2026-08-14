5e2b8ace972f0f9640587834bea06e1446813a9b
Alex Steel <alexsteel@Alexs-MacBook-Air-1433.local>
2026-08-14 15:03:34 +0100

    improving delete matches

--- delete_matches.py
+++ delete_matches.py
@@ -1,4 +1,8 @@
 from predictions.models import Match
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "predictorleague.settings")
+import django
+django.setup()
 def run():
     for match in Match.objects.all():
         match.delete()
